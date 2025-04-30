44f7785c23b2ca0dfa6b56104de0126fa5aac97e
NainaGhosh01 <nainaghosh01@gmail.com>
2025-04-30 17:48:44 +0530

    Update codedeploy.tf

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,30 +1,72 @@
-blue_green_deployment_config {
-  deployment_ready_option {
-    action_on_timeout = "CONTINUE_DEPLOYMENT"
-  }
+resource "aws_iam_role" "codedeploy_role" {
+  name = "strapi-codedeploy-role"
 
-  terminate_blue_instances_on_deployment_success {
-    action = "TERMINATE"
-    termination_wait_time_in_minutes = 5
-  }
-
-  green_fleet_provisioning_option {
-    action = "DISCOVER_EXISTING"
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-load_balancer_info {
-  target_group_pair_info {
-    target_group {
-      name = aws_lb_target_group.strapi_tg_green.name  # Correct reference
+resource "aws_iam_role_policy_attachment" "codedeploy_policy_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+
+resource "aws_codedeploy_app" "strapi" {
+  name             = "strapi-codedeploy-app"
+  compute_platform = "ECS"
+}
+
+resource "aws_codedeploy_deployment_group" "strapi" {
+  app_name               = aws_codedeploy_app.strapi.name
+  deployment_group_name  = "strapi-deployment-group"
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
     }
 
-    target_group {
-      name = aws_lb_target_group.strapi_tg_blue.name   # Correct reference
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
     }
 
-    prod_traffic_route {
-      listener_arns = [aws_lb_listener.http_listener.arn]
+    green_fleet_provisioning_option {
+      action = "DISCOVER_EXISTING"
+    }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.strapi_cluster.name
+    service_name = aws_ecs_service.strapi_service.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      target_group {
+        name = aws_lb_target_group.strapi_tg_green.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.strapi_tg_blue.name
+      }
+
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http_listener.arn]
+      }
     }
   }
 }

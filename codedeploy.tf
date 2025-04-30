8262032c7e97711e733b899670ce220fdfa8924e
NainaGhosh01 <nainaghosh01@gmail.com>
2025-04-30 14:20:12 +0530

    Update codedeploy.tf

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,73 +1,30 @@
-# CodeDeploy Application for ECS
-resource "aws_codedeploy_app" "strapi_app" {
-  name = "strapi-codedeploy-app"
-  compute_platform = "ECS"
-}
-
-# IAM Role for CodeDeploy
-resource "aws_iam_role" "strapi_codedeploy_role" {
-  name = "strapi-codedeploy-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "codedeploy.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach AWS managed CodeDeploy role policy
-resource "aws_iam_role_policy_attachment" "codedeploy_policy_attach" {
-  role       = aws_iam_role.strapi_codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-}
-
-# CodeDeploy Deployment Group for ECS Blue/Green
-resource "aws_codedeploy_deployment_group" "strapi" {
-  app_name              = aws_codedeploy_app.strapi_app.name
-  deployment_group_name = "strapi-deployment-group"
-  service_role_arn      = aws_iam_role.strapi_codedeploy_role.arn
-
-  deployment_style {
-    deployment_type = "BLUE_GREEN"
-    deployment_option = "WITH_TRAFFIC_CONTROL"
+blue_green_deployment_config {
+  deployment_ready_option {
+    action_on_timeout = "CONTINUE_DEPLOYMENT"
   }
 
-  blue_green_deployment_config {
-    terminate_blue_instances_on_deployment_success {
-      action = "TERMINATE"
-      termination_wait_time_in_minutes = 5
+  terminate_blue_instances_on_deployment_success {
+    action = "TERMINATE"
+    termination_wait_time_in_minutes = 5
+  }
+
+  green_fleet_provisioning_option {
+    action = "DISCOVER_EXISTING"
+  }
+}
+
+load_balancer_info {
+  target_group_pair_info {
+    target_group {
+      name = aws_lb_target_group.strapi_tg_green.name  # Correct reference
     }
 
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-      wait_time_in_minutes = 5
+    target_group {
+      name = aws_lb_target_group.strapi_tg_blue.name   # Correct reference
     }
-  }
 
-  ecs_service {
-    cluster_name = aws_ecs_cluster.strapi_cluster.name
-    service_name = aws_ecs_service.strapi_service.name
-  }
-
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.http_listener.arn]
-      }
-
-      target_group {
-        name = aws_lb_target_group.green_target_group.name
-      }
-
-      target_group {
-        name = aws_lb_target_group.blue_target_group.name
-      }
+    prod_traffic_route {
+      listener_arns = [aws_lb_listener.http_listener.arn]
     }
   }
 }
